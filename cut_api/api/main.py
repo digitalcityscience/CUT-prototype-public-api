9a6cb8daba043a46698d832c31200b677eb16235
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-10-07 10:18:17 +0200

    Extract forward call

--- cut_api/api/main.py
+++ cut_api/api/main.py
@@ -6,9 +6,6 @@ from fastapi import FastAPI, Request, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from slowapi import _rate_limit_exceeded_handler
-from slowapi.errors import RateLimitExceeded
-from slowapi.middleware import SlowAPIMiddleware
 
 from cut_api.auth.tokens import AuthError, authorise_request
 from cut_api.config import settings
@@ -45,19 +42,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Set Rate Limiter
-app.state.limiter = LIMITER
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
-app.add_middleware(SlowAPIMiddleware)
-
 
 @app.get("/health_check", tags=["ROOT"])
 async def health_check():
     return "ok"
 
 
+# If target server is not present in this routing table
+# the API will return a 404 - Not Found
 ROUTING_TABLE = {
-    "auth": "http://localhost:8001",
     "resources": "http://localhost:8001",
     "noise": "http://localhost:8002",
     "water": "http://localhost:8003",
@@ -91,17 +84,44 @@ ROUTING_TABLE = {
 # TODO conversion between PNG and GEOJSON
 
 
-@app.middleware("http")
-async def custom_reverse_proxy(request: Request, call_next):
-    if not await LIMITER.can_pass_request(request, rate_per_minute=1):
+async def forward_request(request: Request, target_url: str):
+    if not await LIMITER.can_pass_request(request, rate_per_minute=10):
         return JSONResponse(
             status_code=status.HTTP_429_TOO_MANY_REQUESTS,
             content=CutApiErrorResponse(message="Request limit reached.").dict(),
         )
 
+    # TODO register event here
+
+    async with httpx.AsyncClient() as client:
+        # TODO if token - here all requests must have a token though - but handle errors
+        token = request.headers.get("authorization").replace("Bearer ", "")
+        try:
+            user = authorise_request(token)
+            print(user)
+
+            response = await client.request(
+                request.method, target_url, data=await request.body()
+            )
+
+            return Response(
+                content=response.content,
+                status_code=response.status_code,
+                headers=response.headers,
+            )
+        except AuthError as exc:
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content=CutApiErrorResponse(message=exc.message).dict(),
+            )
+
+
+@app.middleware("http")
+async def custom_reverse_proxy(request: Request, call_next):
     request_path = request.url.path
     print(f"Request path is {request_path}")
 
+    # TODO deactivate docs redocs etc
     if request_path in ["/openapi.json", "/docs", "/"]:
         return await call_next(request)
 
@@ -110,31 +130,10 @@ async def custom_reverse_proxy(request: Request, call_next):
 
     if target_server_url := ROUTING_TABLE.get(target_server_name):
         print(f"Target server URL is {target_server_url}")
-
         target_url = f"{target_server_url}{request_path}"
         print(f"Target endpoint is {target_url}")
+        return await forward_request(request, target_url)
 
-        async with httpx.AsyncClient() as client:
-            # TODO if token - here all requests must have a token though - but handle errors
-            token = request.headers.get("authorization").replace("Bearer ", "")
-            try:
-                user = authorise_request(token)
-                print(user)
-
-                response = await client.request(
-                    request.method, target_url, data=await request.body()
-                )
-
-                return Response(
-                    content=response.content,
-                    status_code=response.status_code,
-                    headers=response.headers,
-                )
-            except AuthError as exc:
-                return JSONResponse(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    content=CutApiErrorResponse(message=exc.message).dict(),
-                )
     return await call_next(request)
 
 
